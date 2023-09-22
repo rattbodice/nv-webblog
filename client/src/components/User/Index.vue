2b6317705ea26e64da1f4ce1c3944a7961c77043
ICE Error <94098012+rattbodice@users.noreply.github.com>
2023-09-22 15:10:18 +0700

    update 123

---
+++ client/src/components/User/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Get All Users</h1>
+    </div>
+</template>
+
+
+<script>
+import UsersService from '@/services/UsersService'
+// import UserService from '../../../../server/src/services/UserService';
+
+export default {
+    async created () {
+        let result = (await UsersService.index()).data
+        console.log(result);
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
+import UserIndex from '@/components/User/Index'
+import UserCreate from '@/components/User/CreateUser'
+import UserEdit from '@/components/User/EditUser'
+import UserShow from '@/components/User/ShowUser'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/users',
+      name: 'users',
+      component: UserIndex
+    },
+    {
+      path: '/user/create',
+      name: "user-create",
+      component: UserCreate
+    },
+    {
+      path: '/user/edit',
+      name: "user-edit",
+      component: UserEdit
+    },
+    {
+      path: '/user/show',
+      name: "user-show",
+      component: UserShow
     }
+    
   ]
 })
